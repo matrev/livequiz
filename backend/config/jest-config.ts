4e6cab65436916e8a159b13622a0073a71013d27
Mark Trevino <32989053+matrev@users.noreply.github.com>
2026-02-04 23:16:39 -0600

    moving jest-config to config folder

--- backend/config/jest-config.ts
+++ backend/config/jest-config.ts
@@ -1,15 +1,14 @@
 import type {Config} from 'jest';
-import { createDefaultPreset } from 'ts-jest';
-const tsJestTransformCfg = createDefaultPreset().transform;
 
 const config: Config = {
     verbose: true,
-    preset: "ts-jest/presets/default-esm",
+    preset: "ts-jest",
     testEnvironment: "node",
     moduleNameMapper: {
         "^(\\.{1,2}/.+)\\.js$": "$1",
     },
     testPathIgnorePatterns: ["/dist/"],
+    rootDir: "../",
 };
 
 export default config;
