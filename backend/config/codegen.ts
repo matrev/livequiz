b5a0e2464f69f6b5a2d15c39cc896ad5b1da5865
Mark Trevino <32989053+matrev@users.noreply.github.com>
2026-02-04 23:26:27 -0600

    moving more config to /config

---
+++ backend/config/codegen.ts
@@ -0,0 +1,18 @@
+
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "./src/graphql/schema.graphql",
+  generates: {
+    "generated/graphql.ts": {
+      plugins: ["typescript", "typescript-resolvers"]
+    },
+  },
+  config: {
+    contextType: "../dist/src/index.js#PrismaContext"
+  }
+
+};
+
+export default config;
