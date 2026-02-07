8b494c4f344e55aa968e41941165fce58ef31bb9
Mark Trevino <32989053+matrev@users.noreply.github.com>
2026-02-06 18:49:27 -0600

    fixed server typings

--- backend/config/codegen.ts
+++ backend/config/codegen.ts
@@ -8,15 +8,6 @@ const config: CodegenConfig = {
     "generated/graphql.ts": {
       plugins: ["typescript", "typescript-resolvers"]
     },
-    "../frontend/src/graphql/": { 
-      // Generates TypeScript types and React hooks for frontend,
-      //  this will need to be updated when we deploy to have the frontend pull from the domain rather than local files
-      preset: "client",
-      plugins: [
-        'typescript-operations',
-        'typescript-react-apollo'
-      ]
-    }
   },
   config: {
     contextType: "../dist/src/index.js#PrismaContext"
