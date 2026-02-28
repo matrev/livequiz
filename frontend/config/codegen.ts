2f2326d6f8c7ce262049cc3470d3352ea34bb3f3
Mark Trevino <32989053+matrev@users.noreply.github.com>
2026-02-28 17:13:32 -0600

    pulling schema from env variable

--- frontend/config/codegen.ts
+++ frontend/config/codegen.ts
@@ -1,7 +1,18 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
+import { loadEnvConfig } from "@next/env";
+
+loadEnvConfig(process.cwd());
+
+const graphqlHttpUrl = process.env.NEXT_PUBLIC_GRAPHQL_HTTP_URL;
+
+if (!graphqlHttpUrl) {
+    throw new Error(
+        "Missing NEXT_PUBLIC_GRAPHQL_HTTP_URL. Set it in or frontend/.env."
+    );
+}
 
 const config: CodegenConfig = {
-    schema: "http://localhost:4000/graphql", //change to local file path if server is not running
+    schema: graphqlHttpUrl,
     documents: ["./src/**/*.{ts,tsx,js,jsx,graphql}"],
     generates: {
         "./src/generated/": {
